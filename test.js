c18c42b20e9105cfae5ab570609ccc988aab2545
JamesNSinclair <53437429+JamesNSinclair@users.noreply.github.com>
2020-09-02 15:29:12 +0100

    all updates to button css

--- test.js
+++ test.js
@@ -1,29 +1,5 @@
-const marketing = document.querySelector('.marketing');
-  const leader = document.querySelector('.leader');
-const staff = document.querySelectorAll('.staff');
+const cross = document.getElementById('moved-cross');
 
-
-marketing.onmouseover = function() {
-    leader.style.transform = "translateX(-400px)";
-        leader.style.transitionDelay = "";
-      for (i = 0; i<staff.length; i++) {
-        let segments = 800/staff.length;
-      let moveValue = segments * (i);
-      let staffSegments = moveValue + 'px';
-          staff[i].style.transitionDelay = "";
-      staff[i].style.visibility = "visible !important";
-      staff[i].style.transform = "translateX(" + `${staffSegments}` + ")";
-  };
-};
-
-
-marketing.onmouseout = function() {
-  leader.style.transitionDelay = "1.05s";
-  leader.style.transform = "";
- for (i = 0; i<staff.length; i++) {
-staff[i].style.transitionDelay = "0";
- staff[i].style.visibility = "";
- staff[i].style.transform = "";
-
-   };
-};
+cross.addEventListener('click', () => {
+  document.getElementById('outer-moving').style.display = "none";
+});
